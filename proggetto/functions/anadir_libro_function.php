90664c15124d9760ffe1e8296951aaad160f7b09
barearojo <barearojojuan@gmail.com>
2024-05-28 21:26:49 +0200

    function añadir libro 2

--- proggetto/functions/anadir_libro_function.php
+++ proggetto/functions/anadir_libro_function.php
@@ -3,17 +3,37 @@
 $host = "postgres";
 $dbname = "juan_barearojo_proggetto";
 $user = "juan_barearojo";
-$password = "Jbr_02062003+(SQL)"
+$password = "Jbr_02062003+(SQL)";
 
-db = pg_connect("host=$host dbanme= $dbname user=$user password=$password")
+db = pg_connect("host=$host dbanme= $dbname user=$user password=$password");
 if(!db){
-    die("Conexión a la base de datos fallida: " .preg_last_error())
+    die("Conexión a la base de datos fallida: " .pg_last_error());
 }
 
 //obtener datos de los formularios
-$titulo = $_POST['titulo']
-$autor = $_POST['autor']
-$edicion = $_POST['edicion']
-$descripcion = $_POST['descripcion']
-$genero = $_POST['genero']
+$titulo = $_POST['titulo'];
+$autor = $_POST['autor'];
+$edicion = $_POST['edicion'];
+$descripcion = $_POST['descripcion'];
+$genero = $_POST['genero'];
+
+$sql = "INSERT INTO libros_table(titulo,autor,edición,descripcion,genero) VALUES($1,$2,$3,$4,$5)";
+$result = pg_prepare($db,"insert_sql",$sql);
+
+if($result == False){
+    die("Fallo al preparar el query".pg_last_error());
+}
+
+$result = pg_execute($db,"login_query",array($titulo,$autor,$edicion,$descripcion,$genero))
+if($result == False){
+    die("Fallo al ejecutar el query".pg_last_error());
+}
+else{
+    echo" exito"
+}
+// Liberar el resultado y cerrar la conexión
+pg_free_result($result);
+pg_close($db);
+
+
 ?>
