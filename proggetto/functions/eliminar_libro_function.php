c19e4778587f54ae82f390e9fa5e40e15e4d95bb
barearojo <barearojojuan@gmail.com>
2024-05-29 16:57:55 +0200

    renombrado

--- proggetto/functions/eliminar_libro_function.php
+++ proggetto/functions/eliminar_libro_function.php
@@ -5,20 +5,22 @@ $dbname = "juan_barearojo_proggetto";
 $user = "juan_barearojo";
 $password = "Jbr_02062003+(SQL)";
 
-$db = pg_connect("host = $host dbname=$dbname user=$user password=$password");
-if(!db){
-    die("Connessione al database fallita:", . pg_last_error());
+$db = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+if (!$db) {
+    die("Connessione al database fallita: " . pg_last_error());
 }
+
 $codigo = $_POST['codigo'];
 
-$sql = DELETE FROM libros_table WHERE codigo = $1;
-$result = pg_prepare($db, "insert_sql", $sql);
+$sql = "DELETE FROM libros_table WHERE codigo = $1";
+$result = pg_prepare($db, "delete_sql", $sql);
 
 if ($result === false) {
     die("Fallo al preparar el query: " . pg_last_error());
 }
 
-$result = pg_execute($db, "insert_sql", array($titulo, $autor, $edicion, $descripcion, $genero));
+$result = pg_execute($db, "delete_sql", array($codigo));
 if ($result === false) {
     die("Fallo al ejecutar el query: " . pg_last_error());
 } else {
