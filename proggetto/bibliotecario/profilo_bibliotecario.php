3b646fc659081436b8dbef9f7c4667d8915023fe
barearojo <barearojojuan@gmail.com>
2024-06-03 10:09:58 +0200

    profilo bibliotecario

--- proggetto/bibliotecario/profilo_bibliotecario.php
+++ proggetto/bibliotecario/profilo_bibliotecario.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+$username = $_SESSION['username']
+$password = $_SESSION['password']
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +13,8 @@
     </head>
     <body>
         <H1>Il tuo profilo</H1>
+        <h2>Il tuo Username: <?php echo $username;?></h2>
+        <h2>Il tuo Password: <?php echo $password;?></h2>
     </body>
 
 </html>
