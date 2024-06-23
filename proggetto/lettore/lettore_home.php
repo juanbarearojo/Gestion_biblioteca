de58009f2f3db3bcb7e530de162846b0fa35f422
barearojo <barearojojuan@gmail.com>
2024-06-23 17:08:37 +0200

    añadido boton profilo_bibliotecario 2

--- proggetto/lettore/lettore_home.php
+++ proggetto/lettore/lettore_home.php
@@ -1,21 +1,33 @@
+<?php
+session_start(); // Iniciar la sesión antes de cualquier salida
+
+$username = isset($_SESSION['username']) ? $_SESSION['username'] : 'bibliotecario';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/css/uikit.min.css" />
-    <title>home_lettore</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <title>Home Lettore</title>
 </head>
 <body>
-    <div class="uk-container uk-margin-top">
-        <h1 class="uk-heading-line"><span>ciao lettore</span></h1>
+    <div class="container mt-5">
+        <h1 class="display-4 text-center">Ciao <?php echo $username;?></span></h1>
 
-        <form action="reservas_usuario.php" method="get" class="uk-form-stacked">
-            <div class="uk-margin">
-                <input class="uk-button uk-button-primary" type="submit" value="Ver tus reservas">
+        <form action="reservas_usuario.php" method="get" class="mt-4">
+            <div class="form-group text-center">
+                <input class="btn btn-primary btn-lg" type="submit" value="Ver tus reservas">
+            </div>
+        </form>
+        <form action="profilo_lettore.php" method="get" class="mt-4">
+            <div class="form-group text-center">
+                <input class="btn btn-primary btn-lg" type="submit" value="Profile">
             </div>
         </form>
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/js/uikit-icons.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
