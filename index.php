5d97318d7ea7d2263676ebe84e6904fb7df9fca5
barearojo <barearojojuan@gmail.com>
2024-06-23 16:39:28 +0200

    cambio a boostrap

--- index.php
+++ index.php
@@ -3,41 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <!-- Incluir CSS de UIKit -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/css/uikit.min.css" />
+    <!-- Incluir CSS de Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 </head>
 <body>
-    <div class="uk-container uk-margin-top">
+    <div class="container mt-5">
         <h1>Login</h1>
-        <form action="progetto/functions/login.php" method="post" class="uk-form-stacked">
-            <div class="uk-margin">
-                <label class="uk-form-label" for="username">Username:</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input" type="text" id="username" name="username" required>
-                </div>
+        <form action="progetto/functions/login.php" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input class="form-control" type="text" id="username" name="username" required>
             </div>
-            <div class="uk-margin">
-                <label class="uk-form-label" for="password">Password:</label>
-                <div class="uk-form-controls">
-                    <input class="uk-input" type="password" id="password" name="password" required>
-                </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input class="form-control" type="password" id="password" name="password" required>
             </div>
-            <div class="uk-margin">
-                <label class="uk-form-label" for="role">Role:</label>
-                <div class="uk-form-controls">
-                    <select class="uk-select" id="role" name="role" required>
-                        <option value="lettore">Lettore</option>
-                        <option value="bibliotecario">Bibliotecario</option>
-                    </select>
-                </div>
+            <div class="form-group">
+                <label for="role">Role:</label>
+                <select class="form-control" id="role" name="role" required>
+                    <option value="lettore">Lettore</option>
+                    <option value="bibliotecario">Bibliotecario</option>
+                </select>
             </div>
-            <div class="uk-margin">
-                <input class="uk-button uk-button-primary" type="submit" value="Login">
+            <div class="form-group">
+                <input class="btn btn-primary" type="submit" value="Login">
             </div>
         </form>
     </div>
-    <!-- Incluir JS de UIKit -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/js/uikit-icons.min.js"></script>
+    <!-- Incluir JS de Bootstrap y sus dependencias -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
