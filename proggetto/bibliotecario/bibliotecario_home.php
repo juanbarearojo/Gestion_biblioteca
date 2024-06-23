36f873fdec43f25c37e838a1adb67400f447f423
barearojo <barearojojuan@gmail.com>
2024-06-23 16:41:54 +0200

    cambios home a boostrap

--- proggetto/bibliotecario/bibliotecario_home.php
+++ proggetto/bibliotecario/bibliotecario_home.php
@@ -8,25 +8,24 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'bibliotecari
     <head>
         <meta charset="UTF-8">
         <title>Home_Bibliotecario</title>
-        <!-- Incluir CSS de UIKit -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/css/uikit.min.css" />
+        <!-- Incluir CSS de Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     </head>
     <body>
-        <div class="uk-container uk-margin-top">
-            <h1 class="uk-heading-line"><span>Ciao, <?php echo $username; ?>!</span></h1>
+        <div class="container mt-5">
+            <h1 class="display-4">Ciao, <?php echo $username; ?>!</h1>
 
-            <form action="gestion_libros.php" method="get" class="uk-form-stacked uk-margin">
-                <input class="uk-button uk-button-primary" type="submit" value="Gestionar libros">
+            <form action="gestion_libros.php" method="get" class="my-4">
+                <input class="btn btn-primary btn-lg btn-block" type="submit" value="Gestionar libros">
             </form>
-            <form action="profilo_bibliotecario.php" method="get" class="uk-form-stacked uk-margin">
-                <input class="uk-button uk-button-primary" type="submit" value="Il tuo profilo">
+            <form action="profilo_bibliotecario.php" method="get" class="my-4">
+                <input class="btn btn-primary btn-lg btn-block" type="submit" value="Il tuo profilo">
             </form>
         </div>
         
-        <!-- Incluir JS de UIKit -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/js/uikit.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.22/js/uikit-icons.min.js"></script>
+        <!-- Incluir JS de Bootstrap y sus dependencias -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </body>
-</html>
-
 </html>
