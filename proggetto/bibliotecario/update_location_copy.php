aa693c18ffc8a9cf2d5107a71c6ee4d6559d2e09
barearojo <barearojojuan@gmail.com>
2024-06-24 14:11:15 +0200

    update location copy

---
+++ proggetto/bibliotecario/update_location_copy.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update location copy</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+    <h1 class="mb-4">Update one copy location</h1>
+    <form action="../functions/update_location_copy_function.php" method="post">
+        <div class="form-group">
+            <label for="ISBN">ISBN:</label>
+            <input type="text" class="form-control" id="isbn" name="isbn" required>
+        </div>
+        <div class="form-group">
+            <label for="library_id">Old library id:</label>
+            <input type="text" class="form-control" id="old_library_id" name="old_library_id" required>
+        </div>
+        <div class="form-group">
+            <label for="library_id">New library id:</label>
+            <input type="text" class="form-control" id="new_library_id" name="new_library_id" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Update location from one copy</button>
+    </form>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ proggetto/functions/update_location_copy_function.php
@@ -0,0 +1,42 @@
+<?php
+
+$host = "postgres";
+$dbname = "juan_barearojo_proggetto";
+$user = "juan_barearojo";
+$password = "Jbr_02062003+(SQL)";
+
+$db = pg_connect("host=$host dbname=$dbname user=$user password=$password");
+
+if (!$db) {
+    die("Connessione al database fallita: " . pg_last_error());
+}
+
+$isbn = $_POST['isbn'];
+$old_library_id = $_POST['old_library_id'];
+$new_library_id = $_POST['new_library_id'];
+
+$sql = "UPDATE copy SET library_id = $3 WHERE copy_id = (
+            SELECT copy_id 
+            FROM copy 
+            WHERE isbn = $1 AND library_id = $2 
+            LIMIT 1
+        )";
+$result = pg_prepare($db, "update_sql", $sql);
+
+if ($result === false) {
+    die("Fallo al preparar el query: " . pg_last_error());
+}
+
+$result = pg_execute($db, "update_sql", array($isbn, $old_library_id, $new_library_id));
+if ($result === false) {
+    die("Fallo al ejecutar el query: " . pg_last_error());
+} else {
+    echo "<script>alert('Copy location updated without errors');</script>";
+    echo "<script>window.location.href = '../bibliotecario/manage_books.php';</script>";
+}
+
+// Liberar el resultado y cerrar la conexión
+pg_free_result($result);
+pg_close($db);
+
+?>
